2af2577f592d229812962b95052f766e812a6619
Cecelia Hahn <ccrhahn17@gmail.com>
2024-08-31 14:33:59 -0500

    Create 1.1_IntroduceYourself.java

---
+++ Unit 1/1.1_IntroduceYourself.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Cecelia Hahn");
+		System.out.println("Junior");
+		System.out.println("I enjoy going on walks with my friends");
+		System.out.print("I chose to take AP computer science because it is ");
+		System.out.print("interesting to me and I enjoyed Intro to Programming.");
+	}
+}
