094fb93c03c4d6a8523629fa1be9445dac6d0384
Cecelia Hahn <ccrhahn17@gmail.com>
2024-08-31 14:35:31 -0500

    Create 1.2_Variables.java

---
+++ Unit 1/1.2_Variables.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		double age = 16 ;
+		int grade = 11 ;
+		String fname = "Cecelia" ;
+		String lname = "Hahn" ;
+		System.out.println("Hello my name is " + fname + " " + lname);
+		System.out.println("I am " + age + " years old and I am in " + grade + "th grade");
+	}
+}
