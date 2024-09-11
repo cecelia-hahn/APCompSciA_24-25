9fcc526eac75327acf6e2b50c5d2f06d0d6faaa6
Cecelia Hahn <ccrhahn17@gmail.com>
2024-09-11 15:50:15 -0500

    Create Apple.java

---
+++ Unit 2/Apple Class/Apple.java
@@ -0,0 +1,13 @@
+public class Apple{
+    private double size;
+    private String color;
+    private boolean isSweet;
+   
+public Apple (double s, String c, boolean i){
+    size = s;
+    color = c;
+    isSweet = i;
+}
+   
+   
+}
