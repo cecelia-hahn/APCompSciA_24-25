8814b94f06ae1d9b53f06c2559e7577938b2bc5d
Cecelia Hahn <ccrhahn17@gmail.com>
2024-11-20 20:17:37 -0600

    Create 4.4_MakePoints.java

---
+++ Unit 4/4.4_MakePoints.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		for (int x = 1; x<6; x++){
+		    for (int y=1; y<6; y++){
+		        System.out.print("(" + x+ "," + y +")");
+		    }
+		  System.out.println("");
+		}
+	}
+}
