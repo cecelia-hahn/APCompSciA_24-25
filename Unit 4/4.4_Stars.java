d7902467cc4ccc25eeda0bac62a059f13c449d93
Cecelia Hahn <ccrhahn17@gmail.com>
2024-12-07 16:35:56 -0600

    Create 4.4_Stars.java

---
+++ Unit 4/4.4_Stars.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+		for (int x = 5; x>0; x--){
+		    for (int y=1; y<=x; y++){
+		        System.out.print("*");
+		    }
+		  System.out.println("");
+		}
+		
+		
+		
+	}
+		
+		
+	}
