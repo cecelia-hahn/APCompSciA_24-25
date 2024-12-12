b34b5ed1818bf47548944ba3cb04b5cb6a365762
Cecelia Hahn <ccrhahn17@gmail.com>
2024-12-12 16:13:22 -0600

    Create SnowMath.java

---
+++ Unit 5/Make Your Own Class Project/SnowMath.java
@@ -0,0 +1,11 @@
+public class SnowMath{
+    public static int coalMouth(int num){
+        return num * 2; //num is how many pieces of coal are used for the mouth
+                        //each piece of coal is about 2 inches long
+    }
+    
+    public static int noseLength(int n){
+        return n - 1; //n is the length of the carrot used for the nose
+                     //1 is subtracted so that the carrot can be pushed into the face far enough
+    }
+}
