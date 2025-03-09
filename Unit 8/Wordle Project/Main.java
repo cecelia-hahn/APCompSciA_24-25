ace92bff7cafd2f49cee5e4dcfb6b4301b7ad02a
Cecelia Hahn <ccrhahn17@gmail.com>
2025-03-09 17:47:12 -0500

    Create Main.java

---
+++ Unit 8/Wordle Project/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		Wordle word = new Wordle("C", "E", "L", "L", "O");
+		//word.input();
+		//word.inputTest();
+		word.runWordle();
+	}
+}
